86a0e5b52423e1263f3b3fe6d70d34e068f8efd0
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2023-01-29 07:27:42 +0300

    Delete tests directory

--- tests/test_capitalize.py
+++
@@ -1,6 +0,0 @@
-from capitalize import capitaize
-
-if capitaize('hello') != 'Hello':
-	raise Exception('Функция работает неверно!')
-if capitaize('') != '':
-	raise Exception('Функция работает неверно!')
