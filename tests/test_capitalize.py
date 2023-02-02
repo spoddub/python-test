621c7c9792aed0aa17c8de2120468471bbd19b1b
spoddub <spoddub98@gmail.com>
2023-02-02 09:15:35 +0300

    updated with assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -2,10 +2,8 @@
 
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') != 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert  capitalize('') != ''
 
 print('Все тесты пройдены!')
